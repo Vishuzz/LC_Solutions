fda5ce989134aaf7932891defc3ee742ed7ea37d
Vishek Tyagi <tyagivishek1234@gmail.com>
2026-08-16 00:44:54 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (48.07%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,9 +1,31 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         int n = nums.length;
-        for(int i=0;i<n;i++){
-            if(target == nums[i]){
+        int low = 0,high = n-1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target){
                 return true;
+            }
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low++;
+                high--;
+            }
+            else if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else{
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
             }
         }
         return false;
