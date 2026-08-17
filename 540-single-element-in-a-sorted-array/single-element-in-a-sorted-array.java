726ae5d16178640943299ac13baaebeaacfeff8d
Vishek Tyagi <tyagivishek1234@gmail.com>
2026-08-18 03:27:25 +0530

    Time: 16 ms (4.71%) | Memory: 56.5 MB (9.00%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,15 +1,14 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        int ans = 0;
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
         for(int x : map.keySet()){
             if(map.get(x) == 1){
-                ans = x;
+                return x;
             }
         }
-        return ans;
+        return -1;
     }
 }
